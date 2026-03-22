c251d7f4b15ad620821dfdfd3f492c537a1a04bb
paulo <paulojosueb58@gmail.com>
2026-03-22 11:45:41 -0600

    actualizacion

--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -3,15 +3,10 @@ import type { Product } from "../types/product";
 
 
 
-interface ProductResponse {
-    products: Product[],
-    total: number,
-    skip:number,
-    limit:number
-}
+
 
 export const getProducts = async():Promise<Product[]>=> {
-    const {data} = await productApi.get<ProductResponse>( '/product')
+    const {data} = await productApi.get( '/product')
 
     return data.products;
 
